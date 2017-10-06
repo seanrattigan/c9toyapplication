a83dd93888e6b398ced9df7e6017ed4b02becc92
Sean Rattigan <x16106776@student.ncirl.ie>
2017-10-06 20:18:30 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def hello
+      render html: "Hello my friends. This will be my toy app when finished!"
+  end
+  
 end
